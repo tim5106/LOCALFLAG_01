8e8da29ef8e222a846d8ad64980961330cf6252e
moonj0503 <moonjoonjang12@gmail.com>
2026-09-17 14:52:18 +0900

    nav.bar

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,8 +1,11 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
+  test: {
+    css: true,
+  },
   plugins: [react(), tailwindcss()],
   server: {
     port: 5173,
